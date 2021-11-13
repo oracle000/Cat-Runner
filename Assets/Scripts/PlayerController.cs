cf33f988b25d409ae74fd5c0915cebf10d79f91d
EQUINOX <john.christopher0092@gmail.com>
2021-11-13 19:17:34 +0800

    fix jump and run animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,14 +7,15 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D _rb;
     private Animator _animator;
-    private Collision2D _collision;
-    private bool IsJumping;
+    private Collider2D _collider;    
+
+    [SerializeField] private LayerMask ground;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
-        _collision = GetComponent<Collision2D>();
+        _collider = GetComponent<Collider2D>();
     }
 
 
@@ -22,31 +23,39 @@ public class PlayerController : MonoBehaviour
     {
         var movement = Input.GetAxis("Horizontal");
         var jumping = Input.GetAxis("Vertical");
-        if (movement > 0)
+
+        if (movement > 0 )
 		{
             transform.Translate(new Vector3(3 * Time.deltaTime, 0, 0), Space.Self);
             transform.localScale = new Vector3(1, 1, 1);
-            _animator.SetBool("AlfredoRun", true);            
-
+            SetJumpRunAnimation(jumping);
         } else if (movement < 0)
 		{           
             transform.Translate(new Vector3(-3 * Time.deltaTime, 0, 0), Space.Self);
             transform.localScale = new Vector3(-1, 1, 1);
-            _animator.SetBool("AlfredoRun", true);            
-        } else if (movement == 0)
+            SetJumpRunAnimation(jumping);            
+        } else if (movement == 0 && jumping == 0)
 		{
             _animator.SetBool("AlfredoIdle", true);            
         }
 
-        if (jumping > 0 && IsJumping == false)
-		{
-            IsJumping = true;
-			_rb.velocity = new Vector2(_rb.velocity.x, 5f);
-            // transform.Translate(new Vector3(0, .5f, 0), Space.Self);
-		} else
-		{
-            IsJumping = false;
-		}
+        if (jumping > 0 && _collider.IsTouchingLayers(ground))
+		{        
+			_rb.velocity = new Vector2(_rb.velocity.x, 5f);            
+            _animator.SetBool("AlfredoJump", true);
+	    } 
+    }
 
+
+    private void SetJumpRunAnimation(float jumping)
+	{
+        if (jumping > 0)
+        {
+            _animator.SetBool("AlfredoJump", true);
+        }
+        else
+        {
+            _animator.SetBool("AlfredoRun", true);
+        }
     }
 }
